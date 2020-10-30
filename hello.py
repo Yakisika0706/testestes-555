870f9d6ced5d95f3d9d664aaa6649503c485fc98
Yakisika0706 <70683353+Yakisika0706@users.noreply.github.com>
2020-10-30 11:07:08 +0900

    gdgdd

--- hello.py
+++ hello.py
@@ -1,9 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("C:/Users/loot shirakagi/Desktop/yugioh regu key/my-ragulation-ygo-firebase-adminsdk-f6nlg-db192fde04.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://my-ragulation-ygo.firebaseio.com',
+    'databaseAuthVariableOverride': {
+        'uid': 'my-service-worker'
+    }
+})
+
+ref = db.reference()
  
 @app.route('/')
 def hello():
-    hello = "Hello world"
+    hello = (ref.get())
     return hello
  
 if __name__ == "__main__":
